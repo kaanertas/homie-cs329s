e0d4b7c211fb324a1f244a5ab0daaae4ba258c9c
Kaan Ertas <kkaanertas@gmail.com>
2021-02-07 19:29:32 -0600

    basic website code

---
+++ website/cloudinary/index.js
@@ -0,0 +1,21 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.config({
+    cloud_name: "dlyjr71kx",
+    api_key: "833231282457385",
+    api_secret: "LEFOthdVyM1Nuyot1fxtAW-xXaw",
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: 'Landmarks',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
+});
+
+module.exports = {
+    cloudinary,
+    storage
+}
