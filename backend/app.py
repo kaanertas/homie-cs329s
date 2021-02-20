0b4fc9f3692b5882bb6e497a72ec4dcfd5b0e953
Kaan Ertas <kkaanertas@gmail.com>
2021-02-19 19:01:41 -0600

    integrate model

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import urllib.request
+from model import Model
 import pickle
 # import model
 
@@ -10,13 +12,16 @@ def predict():
 	#request: url, no of preds
 
 	img_url = request.json['img_url']
+	file_name = 'uploaded.jpg'
+	urllib.request.urlretrieve(img_url, file_name)
 	# no_preds = request.json['no_preds']
-	# output = m.predict(img_url,no_preds)
+	output = m.predict(file_name, 7)
+	#print(output)
 	# boxes, confidence, predicted_classes = output
-	predicted_classes = [1,2]
+	predicted_classes = output
 
 	return jsonify(preds=predicted_classes,img_url=img_url)
 
 if __name__ == "__main__":
-	# m = model.Model()
+	m = Model()
 	app.run(debug=True)
---
+++ backend/model.py
@@ -0,0 +1,87 @@
+import torch, torchvision
+import detectron2
+from detectron2.utils.logger import setup_logger
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import cv2
+import random
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+
+class Model:
+    def __init__(self):
+        self.device = torch.device("cpu")
+        self.cfg = get_cfg()
+        self.cfg.MODEL.DEVICE = 'cpu'
+        self.cfg.merge_from_file("./retinanet_model_final_config.yaml")
+        self.cfg.MODEL.WEIGHTS = "./retinanet_model_final.pth"
+        self.predictor = DefaultPredictor(self.cfg)
+        self.target_classes =  ['Bathtub', 'Bed', 'Billiard table', 'Ceiling fan', 'Coffeemaker', 'Couch', 'Countertop', 'Dishwasher',
+                  'Fireplace','Fountain', 'Gas stove', 'Jacuzzi', 'Kitchen & dining room table' 'Microwave oven',
+                  'Mirror', 'Oven', 'Pillow', 'Porch', 'Refrigerator', 'Shower', 'Sink', 'Sofa bed', 'Stairs',
+                  'Swimming pool', 'Television', 'Toilet', 'Towel', 'Tree house', 'Washing machine', 'Wine rack']
+
+    def predict(self, img_pth, num_amenities):
+        img = cv2.imread(img_pth)
+        # cv2.imshow('image', img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        outputs = self.predictor(img)
+        predicted_class_nums = list(outputs["instances"][:num_amenities].pred_classes)
+        pred_classes = list(set([self.target_classes[idx] for idx in predicted_class_nums]))
+        # print(outputs)
+        visualizer = Visualizer(img_rgb=img[:, :, ::-1],
+                                metadata=MetadataCatalog.get(self.cfg.DATASETS.TEST[0]).set(thing_classes=self.target_classes),
+                                scale=0.7)
+        # Draw the models predictions on the target image
+        v = visualizer.draw_instance_predictions(outputs["instances"][:num_amenities].to("cpu"))
+        output_img = v.get_image()[:, :, ::-1]
+        #print(output_img)
+        cv2.imwrite('output.jpg', output_img)
+
+        return pred_classes
+
+
+# TARGET_CLASSES = ['Bathtub', 'Bed', 'Billiard table', 'Ceiling fan', 'Coffeemaker', 'Couch', 'Countertop', 'Dishwasher',
+#                   'Fireplace','Fountain', 'Gas stove', 'Jacuzzi', 'Kitchen & dining room table' 'Microwave oven',
+#                   'Mirror', 'Oven', 'Pillow', 'Porch', 'Refrigerator', 'Shower', 'Sink', 'Sofa bed', 'Stairs',
+#                   'Swimming pool', 'Television', 'Toilet', 'Towel', 'Tree house', 'Washing machine', 'Wine rack']
+#
+# def load_model():
+#     device = torch.device("cpu")
+#     cfg = get_cfg()
+#     cfg.MODEL.DEVICE = 'cpu'
+#     cfg.merge_from_file("./retinanet_model_final_config.yaml")
+#     cfg.MODEL.WEIGHTS = "./retinanet_model_final.pth"
+#     predictor = DefaultPredictor(cfg)
+#     return predictor, cfg
+#
+# def run_inference(predictor, cfg):
+#     img = cv2.imread('test.jpg')
+#     # cv2.imshow('image', img)
+#     # cv2.waitKey(0)
+#     # cv2.destroyAllWindows()
+#     outputs = predictor(img)
+#     # print(outputs)
+#     num_amenities = 7
+#     visualizer = Visualizer(img_rgb=img[:, :, ::-1], metadata=MetadataCatalog.get(cfg.DATASETS.TEST[0]).set(thing_classes=TARGET_CLASSES), scale=0.7)
+#     # Draw the models predictions on the target image
+#     v = visualizer.draw_instance_predictions(outputs["instances"][:num_amenities].to("cpu"))
+#     output_img = v.get_image()[:, :, ::-1]
+#     print(output_img)
+#     cv2.imwrite('output.jpg', output_img)
+#
+# def main():
+#     predictor, cfg = load_model()
+#     run_inference(predictor, cfg)
+#
+# if __name__ == "__main__":
+#     main()
+
+
+
